84a364dc18ddb86a3737fccf48b7f57172e9aae5
sofalere <sofalere@gmail.com>
2023-07-20 12:49:36 -0500

    port change

--- index.js
+++ index.js
@@ -6,30 +6,11 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-// app.use(morgan('tiny'));
-morgan.token('body', req => {
-  return JSON.stringify(req.body);
-});
+
+morgan.token('body', req => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
 
-
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method);
-  console.log('Path:  ', request.path);
-  console.log('Body:  ', request.body);
-  console.log('---');
-  next();
-};
-
-// app.use(requestLogger);
-
-// const unknownEndpoint = (request, response) => {
-//   response.status(404).send({ error: 'unknown endpoint' });
-// };
-
-// app.use(unknownEndpoint);
-
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
